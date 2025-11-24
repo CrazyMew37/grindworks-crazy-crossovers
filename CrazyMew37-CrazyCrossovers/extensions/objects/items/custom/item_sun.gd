70fd801419de773f26cf58c80cededadcd1cb438
CrazyMew37 <youngmaxwell130@gmail.com>
2025-11-23 20:27:07 -0600

    0.2b

--- CrazyMew37-CrazyCrossovers/extensions/objects/items/custom/item_sun.gd
+++ CrazyMew37-CrazyCrossovers/extensions/objects/items/custom/item_sun.gd
@@ -8,4 +8,4 @@ func on_collect(_item: Item, _object: Node3D) -> void:
 func on_item_removed() -> void:
 	# bye bye boost -cm37
 	for track in Util.get_player().stats.gag_balance.keys():
-		Util.get_player().stats.gag_regeneration[track] += 1
+		Util.get_player().stats.gag_regeneration[track] -= 1
