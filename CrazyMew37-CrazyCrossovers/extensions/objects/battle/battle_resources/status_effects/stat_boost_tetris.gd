d4ddf102b76d0ad7cbc3cd6988335768f9e32270
CrazyMew37 <youngmaxwell130@gmail.com>
2026-01-27 19:13:27 -0600

    1.0b

--- CrazyMew37-CrazyCrossovers/extensions/objects/battle/battle_resources/status_effects/stat_boost_tetris.gd
+++ CrazyMew37-CrazyCrossovers/extensions/objects/battle/battle_resources/status_effects/stat_boost_tetris.gd
@@ -14,7 +14,7 @@ var ICONS := {
 
 @export var stat: String = 'Drop'
 @export var boost: float = 0.2
-var tetris_block: int = 1
+var tetris_block: int = 2
 
 func apply():
 	# Applies a random Tetromino. 1: T/Toon-Up, 2: Z/Squirt, 3: O/Trap, 4: S/Lure, 5: J/Sound, 6: L/Throw, 6: I/Drop
