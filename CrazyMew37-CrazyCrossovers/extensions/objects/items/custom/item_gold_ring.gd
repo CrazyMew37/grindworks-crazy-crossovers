a7ad4456a4f0e70faa6fbbc51087b4e8dd008248
CrazyMew37 <youngmaxwell130@gmail.com>
2026-05-17 17:22:46 -0500

    1.1c

--- CrazyMew37-CrazyCrossovers/extensions/objects/items/custom/item_gold_ring.gd
+++ CrazyMew37-CrazyCrossovers/extensions/objects/items/custom/item_gold_ring.gd
@@ -43,11 +43,17 @@ func first_money_change(money: int) -> void:
 	Util.get_player().stats.max_hp = Util.get_player().stats.max_hp + money_laff
 	Util.get_player().stats.hp = Util.get_player().stats.hp + money_laff
 
+var updating_money := false
+
 func on_money_changed(money: int) -> void:
+	if updating_money:
+		return
+	
+	updating_money = true
 	var money_laff = floori(money / 5)
 	if money_laff > 100 && EndlessNerfsSetting == 0:
 		money_laff = 100
-	Util.get_player().stats.max_hp -= old_bean_amount
-	Util.get_player().stats.max_hp = Util.get_player().stats.max_hp + money_laff
-	Util.get_player().stats.hp = Util.get_player().stats.hp + (money_laff - old_bean_amount)
+	Util.get_player().stats.max_hp += (money_laff - old_bean_amount)
+	Util.get_player().stats.hp += (money_laff - old_bean_amount)
 	grab_old_bean_amount()
+	updating_money = false
