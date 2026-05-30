40ba9b6a62cd3825cb27e0423e8757b0a271f9a1
CrazyMew37 <youngmaxwell130@gmail.com>
2026-05-30 04:01:26 -0500

    1.2

--- CrazyMew37-CrazyCrossovers/extensions/objects/items/custom/item_emerald.gd
+++ CrazyMew37-CrazyCrossovers/extensions/objects/items/custom/item_emerald.gd
@@ -3,5 +3,5 @@ extends ItemScript
 const EARN_SFX := preload("res://audio/sfx/ui/tick_counter.ogg")
 
 func on_collect(_item: Item, _object: Node3D) -> void:
-	Util.get_player().stats.add_money(25)
+	Util.get_player().stats.add_money(35)
 	AudioManager.play_sound(EARN_SFX)
